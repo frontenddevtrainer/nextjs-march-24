67456e2e72acc6b313edf37b096559843a7d46a6
Admin <admin@example.com>
2024-03-13 12:02:39 +0000

    Used map to create list of cards.

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,31 @@ import Heading from "@/app/components/heading";
 import Card from "./components/card";
 
 export default function Home() {
+  const cards = [
+    {
+      heading: "this is the card component",
+      id: 1,
+    },
+    {
+      heading: "This is heading 2",
+      content: "this is the card component 2",
+      id: 2,
+    },
+    {
+      heading: "This is heading 3",
+      content: "this is the card component 3",
+      id: 3,
+    },
+  ];
+
   return (
     <>
       <Heading />
-      <Card heading="this is the card component" />
-      <Card
-        content="This is heading 2"
-        heading="this is the card component 2"
-      />
+      {cards?.length > 0 &&
+        cards.map((card, index) => {
+          const { heading, id, content } = card;
+          return <Card heading={heading} content={content} key={id} />;
+        })}
     </>
   );
 }
