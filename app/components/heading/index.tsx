6cf671096fb7ea53ff6a3ba6bce9c0fc5fbe029f
Admin <admin@example.com>
2024-03-13 11:10:29 +0000

    JSX attr and styles

--- app/components/heading/index.tsx
+++ app/components/heading/index.tsx
@@ -1,5 +1,25 @@
-const Heading = ()=>{
-    return <h1  className="text-3xl">Hello World</h1>
-}
+"use client";
+
+const Heading = () => {
+  const message = "hello world";
+  const id = "heading-12";
+  const textColor = "red";
+
+  const clickHandler = () => {
+    console.log("hello world is clicked");
+  };
+
+  return (
+    <h1
+      style={{ color: Math.random() > 0.5 ? "red" : "blue" }}
+      id={id}
+      onClick={clickHandler}
+      onMouseEnter={clickHandler}
+      className="text-3xl"
+    >
+      {message}
+    </h1>
+  );
+};
 
 export default Heading;
