77e2b7446592f1d6eef27f104d23d61dd799c8a1
Admin <admin@example.com>
2024-03-13 11:47:57 +0000

    intro to states.

--- app/components/card/index.tsx
+++ app/components/card/index.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useState } from "react";
+
 interface ICardProps {
   heading: string;
   content?: string;
@@ -6,13 +10,30 @@ interface ICardProps {
 const Card = (props: ICardProps) => {
   const { heading, content } = props;
 
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="p-10 rounded-lg drop-shadow-sm bg-white">
       <h1>{heading}</h1>
       {/* if condition */}
       {/* { content && <section>{content}</section>}  */}
-
       {content ? <section>{content}</section> : <div>Content not present</div>}
+
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        1+
+      </button>
+      <p>Counter: {counter}</p>
+      <button
+        onClick={() => {
+          setCounter(counter - 1);
+        }}
+      >
+        -1
+      </button>
     </div>
   );
 };
